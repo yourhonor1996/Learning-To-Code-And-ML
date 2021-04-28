2236a89ba664ffe3e56810d5378137acc65a1583
Mohammd Alavi <yourhonor1996@gmail.com>
2021-04-28 19:38:16 +0430

    Answer Data Analysis Excercise 1

--- FCC-Data_Analysis_With_Python_Certificate/1-Pandas-Real-Life-Example-master/Excersises_1_Answers.py
+++ FCC-Data_Analysis_With_Python_Certificate/1-Pandas-Real-Life-Example-master/Excersises_1_Answers.py
@@ -42,3 +42,63 @@ sales[['Customer_Age', 'Country']].boxplot(by= 'Country', figsize= (12,8), )
 # %%
 sales.boxplot(column= 'Customer_Age', by= 'Country', figsize= (10,10))
 # %%
+sales['Product'].value_counts().head(10).plot(kind= 'bar', figsize= (8,8))
+# %%
+sales.plot(kind= 'scatter', x= 'Unit_Cost', y= 'Unit_Price', figsize= (8,8))
+# %%
+sales.corr()
+# %%
+sales[['Unit_Price', 'Unit_Cost']].corr()
+# %%
+sales.plot(kind= 'scatter', x= 'Order_Quantity', y= 'Profit', figsize= (7,7))
+# %%
+sales.boxplot('Profit', 'Country', figsize= (7,7))
+# %%
+sales.boxplot('Customer_Age', 'Country', figsize= (10,7))
+# %%
+sales['Calculated_Date'] = sales[['Year', 'Month', 'Day']].apply(lambda x: f'{x[0]}-{x[1]}-{x[2]}', axis= 1)
+# %%
+sales['Calculated_Date'].head()
+# %%
+sales['Calculated_Date'] = pd.to_datetime(sales['Calculated_Date'])
+# %%
+sales['Calculated_Date'].head()
+
+# %%
+sales['Calculated_Date'].value_counts().plot(kind= 'line', figsize= (20,8))
+# %%
+sales['Revenue'] += 50
+sales['Revenue'].head(10)
+# %%
+sales.loc[(sales['Country'] == 'Canada') | (sales['Country'] == 'France')].shape[0]
+# %%
+sales.loc[(sales['Country'] == 'Canada') & (sales['Sub_Category'] == 'Bike Racks')].shape[0]
+# %%
+france_states = sales.loc[(sales['Country'] == 'France'), 'State'].value_counts()
+# %%
+france_states.plot(kind= 'bar', figsize= (10,7))
+# %%
+sales['Product_Category'].value_counts().plot(kind= 'pie', figsize= (8,8))
+# %%
+accessories = sales.loc[sales['Product_Category'] == 'Accessories', 'Sub_Category'].value_counts()
+accessories
+# %%
+accessories.plot(kind= 'pie', figsize= (7,7))
+# %%
+bikes = sales.loc[sales['Product_Category'] == 'Bikes', 'Sub_Category'].value_counts()
+bikes
+# %%
+bikes.plot(kind= 'pie', figsize= (7,7))
+
+# %%
+sales['Customer_Gender'].value_counts().plot(kind= 'bar', figsize= (5,10))
+# %%
+sales.loc[(sales['Customer_Gender'] == 'M') & (sales['Revenue'] > 500)].shape[0]
+
+# %%
+sales.sort_values('Revenue', ascending= False).head(5)
+# %%
+sales.loc[sales['Revenue'] == sales['Revenue'].max()]
+# %%
+sales.loc[sales['Revenue'] > 10000, 'Order_Quantity'].mean()
+# %%
