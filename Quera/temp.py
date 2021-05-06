bc5239189cb51418bd7e23285ce80fe638d15ba8
Mohammd Alavi <yourhonor1996@gmail.com>
2021-05-06 09:34:50 +0430

    Quera: python advanced course

---
+++ Quera/temp.py
@@ -0,0 +1,63 @@
+#%% 
+import os
+from posix import listdir
+# %%
+os.getcwd()
+# %%
+os.listdir()
+# %%
+os.chdir('Questions')
+# %%
+os.listdir()
+# %%
+os.chdir('..')
+os.getcwd()
+# %%
+os.listdir()
+# %%
+os.chdir('Learning-To-Code-And-ML\\Quera')
+# %%
+os.cpu_count()
+# %%
+os.environ
+# %%
+os.curdir
+# %%
+os.defpath
+# %%
+os.altsep
+# %%
+os.getlogin()
+# %%
+os.getppid()
+# %%
+os.getpid()
+# %%
+os.name
+# %%
+os.pardir
+# %%
+os.times()
+# %%
+os.system('ipconfig')
+# %%
+os.system('E:\\Pycharm Projects\\Programming_And_ML\\Learning-To-Code-And-ML\\Quera\\Questions\\1359-hefze-tartib.py')
+# %%
+os.path.pardir
+# %%
+os.path.curdir
+# %%
+os.listdir()
+# %%
+os.getcwd()
+# %%
+listdir('.')
+# %%
+os.listdir('.')
+# %%
+os.listdir('...')
+# %%
+os.walk('.')
+# %%
+list(os.walk('.'))
+# %%
